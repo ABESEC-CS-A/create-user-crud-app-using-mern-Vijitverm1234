1242601637f7dd2ab61f552691d1168a5934550c
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-04-24 22:50:22 +0530

    added the find func

--- Client/client-side/src/pages/FindUser/FindUser.jsx
+++ Client/client-side/src/pages/FindUser/FindUser.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import './FindUser.css'
 const FindUser = () => {
     const [user,setUser]=useState([])
+    const [searchData,setSearchData]=useState('')
     useEffect(()=>{
         const getUser= async()=>{
             const res= await axios.get('http://localhost:4001/users/users') 
@@ -10,22 +11,27 @@ const FindUser = () => {
           }
           getUser()
     },[])
+    const filteredData=user.filter(user=>user.name.toLowerCase().includes(searchData.toLowerCase()))
   return (
     <div>
-      <h1>Finding the User</h1>
+      <h1>Finding the User 🔎</h1>
       <div>
+        <input type="text" placeholder='enter the name of user 🔎' className='find' value={searchData} onChange={(e)=>setSearchData(e.target.value)}/><h1 className='findhead' >
+
+        </h1>
       <h2>All Users</h2>
-      {user.length === 0 ? (
-        <p>No users found.</p>
-      ) : (
-        <ul>
-          {user.map(user => (
-            <li key={user._id}>
-              <strong>{user.name}</strong> - {user.email}
-            </li>
-          ))}
-        </ul>
-      )}
+      {filteredData.length === 0 ? (
+  <p>No users found.</p>
+) : (
+  <ul>
+    {filteredData.map(user => (
+      <li key={user._id}>
+        <strong>{user.name}</strong> - {user.email}
+      </li>
+    ))}
+  </ul>
+)}
+
     </div>
     </div>
   )
